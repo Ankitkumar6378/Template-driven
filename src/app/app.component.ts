a2572f151d431c6ff991c3150bc463b3ae9ea8cc
Ankit <ankit.kumar637849@gmail.com>
2021-11-29 18:41:48 +0530

    template-driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserdataService } from './services/userdata.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Api-project';
-}
+  title = 'Api-project'; 
+  users:any;
+  constructor(private userdata:UserdataService){
+    userdata.users().subscribe((data)=>{
+      console.warn("data",data);
+      this.users=data});
+  
+  } 
+}  
+           
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { UserdataService } from '../services/userdata.service';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  users:any;
+  constructor(private userdata:UserdataService){
+    userdata.users().subscribe((data)=>{
+      console.warn("data",data);
+      this.users=data});
+  
+  } 
+
+  ngOnInit(): void {
+  }
+
+}
